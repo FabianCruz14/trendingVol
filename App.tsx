5abb859ce88261c74691a63efa1f97be15a90963
FabianCruz14 <fabian.cruzh@alumno.buap.mx>
2025-04-23 11:22:52 -0600

    adding a FAB example

--- App.tsx
+++ App.tsx
@@ -1,94 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View, Platform } from 'react-native';
-import { MenuView, MenuComponentRef } from '@react-native-menu/menu';
-import { useRef } from 'react';
+import * as React from 'react';
+import { StyleSheet, Text, View} from 'react-native';
+import { FAB, Provider as PaperProvider } from 'react-native-paper';
+
 
 export default function App() {
-  const menuRef = useRef<MenuComponentRef>(null);
-  
+  const [open, setOpen] = React.useState(false);
   return (
-    <View style={styles.container}>
-
-      <Button
-        title="Open Menu"
-        onPress={() => {
-          menuRef.current?.show();}}
-      />
-      
-      <MenuView
-        ref={menuRef}
-        title="Menu"
-        onPressAction={({nativeEvent}) => {
-          console.warn(JSON.stringify(nativeEvent));
-        }}
-
-        actions={[
-          {
-            id: 'add',
-            title: 'Add',
-            titleColor: '#2367A2',
-            image: Platform.select({
-              ios: 'plus',
-              android: 'ic_menu_add',
-            }),
-            imageColor: '#2367A2',
-            subactions: [
-              {
-                id: 'nested1',
-                title: 'Nested action',
-                titleColor: 'rgba(250,180,100,0.5)',
-                subtitle: 'State is mixed',
-                image: Platform.select({
-                  ios: 'heart.fill',
-                  android: 'ic_menu_today',
-                }),
-                imageColor: 'rgba(100,200,250,0.3)',
-                state: 'mixed',
-              },
-              {
-                id: 'nestedDestructive',
-                title: 'Destructive Action',
-                attributes: {
-                  destructive: true,
-                },
-                image: Platform.select({
-                  ios: 'trash',
-                  android: 'ic_menu_delete',
-                }),
-              },
-            ],
-          },
-          {
-            id: 'share',
-            title: 'Share Action',
-            titleColor: '#46F289',
-            subtitle: 'Share action on SNS',
-            image: Platform.select({
-              ios: 'square.and.arrow.up',
-              android: 'ic_menu_share',
-            }),
-            imageColor: '#46F289',
-            state: 'on',
-          },
-          {
-            id: 'destructive',
-            title: 'Destructive Action',
-            attributes: {
-              destructive: true,
-            },
-            image: Platform.select({
-              ios: 'trash',
-              android: 'ic_menu_delete',
-            }),
-          },
-        ]}
-        shouldOpenOnLongPress={false}
-      >
-        <View style={styles.button}>
-          <Text style={styles.buttonText}>Test</Text>
-        </View>
-      </MenuView>
-    </View>
+    <PaperProvider> 
+      <View style={styles.container}>
+        <Text>Welcome to the App!</Text>
+        <FAB
+          style={styles.button}
+          icon="bell"
+          label="Add"
+          onPress={() => setOpen(!open)}  
+        />
+      </View>
+    </PaperProvider>
   );
 }
 
@@ -97,21 +25,19 @@ const styles = StyleSheet.create({
     padding: 20,
     flex: 1,
     marginTop: 50,
-    height: '75%',
+    height: '100%',
     width: '100%',
     backgroundColor: 'cyan',
   },
   button : {
+    margin: 'auto',
+    marginTop: 20,
     backgroundColor: 'blue',
     width: 50,
-    height: 50,
+    height: 100,
     borderRadius: 25,
+    flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  buttonText: {
-    fontSize: 16,
-    color: 'white',
-    textAlign: 'center',
   },
 });

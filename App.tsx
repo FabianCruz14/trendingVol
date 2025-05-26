056e3e85a89d3d3900e3f607078e79e90c345b46
FabianCruz14 <fabian.cruzh@alumno.buap.mx>
2025-05-26 17:32:38 -0600

    adding a safeareaview

--- App.tsx
+++ App.tsx
@@ -2,16 +2,19 @@ import * as React from 'react';
 import { StyleSheet, Text, View} from 'react-native';
 import Header from './layout/header/Header';
 import Buttons from './components/DoublePositiveButton';
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaProvider>
+      <SafeAreaView style = { styles.container}>
       <Header/>
-
       <Buttons name='Fabian Cruz' action='Serve'></Buttons>
 
-    </View>
+      </SafeAreaView>
+
+    </SafeAreaProvider>
   );
 }
 

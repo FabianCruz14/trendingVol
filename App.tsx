0513dd9cf4aaddd622c8ca1edf031116e3b7deba
FabianCruz14 <fabian.cruzh@alumno.buap.mx>
2025-06-30 16:57:49 -0600

    improving a functions to calcule Frecuencies

--- App.tsx
+++ App.tsx
@@ -2,14 +2,15 @@ import * as React from 'react';
 import { StyleSheet, Text, View} from 'react-native';
 import Header from './layout/header/Header';
 import MainButton from './layout/app/MainButton';
-import { calculateFrecuancies } from './functions/AbsoluteFrequency';
+import { avgDoubleNegative, avgNegative } from './functions/AbsoluteFrequency';
 
 // examples
 // from any array of values, get a frequency table with this values
 
 // 1.- [-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-1,-2,-1,-2]
 const AttacksFabian = [-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-1,-2,-1,-2];
-const absoluteFrecuencyFabian = calculateFrecuancies(AttacksFabian);
+const avgFabianDN = avgDoubleNegative(AttacksFabian);
+const avgFabianN = avgNegative(AttacksFabian);
 
 export default function App() {
   return (
@@ -29,16 +30,46 @@ export default function App() {
 
       <View style={styles.separator} />
 
+      {/* in this column we show the absolute frecuency of all values */}
         <View>
-          <Text>{absoluteFrecuencyFabian.doubleNegative}</Text>
-          <Text>{absoluteFrecuencyFabian.negative}</Text>
-          <Text>{absoluteFrecuencyFabian.zero}</Text>
+          <Text>{avgFabianDN.absolute}</Text>
+          <Text>{avgFabianN.absolute}</Text>
+          {/* <Text>{absoluteFrecuencyFabian.zero}</Text>
           <Text>{absoluteFrecuencyFabian.positive}</Text>
-          <Text>{absoluteFrecuencyFabian.doublePositive}</Text>
+          <Text>{absoluteFrecuencyFabian.doublePositive}</Text> */}
         </View>
 
       <View style={styles.separator} />
 
+        <View>
+          <Text>{avgFabianDN.relative}</Text>
+          <Text>{avgFabianN.relative}</Text>
+          {/* <Text>{absoluteFrecuencyFabian.negative}</Text>
+          <Text>{absoluteFrecuencyFabian.zero}</Text>
+          <Text>{absoluteFrecuencyFabian.positive}</Text>
+          <Text>{absoluteFrecuencyFabian.doublePositive}</Text> */}
+        </View>
+      
+      <View style={styles.separator} />
+
+        <View>
+            <Text>{avgFabianDN.parcial}</Text>
+            <Text>{avgFabianN.parcial}</Text>
+            {/* <Text>{absoluteFrecuencyFabian.negative}</Text>
+            <Text>{absoluteFrecuencyFabian.zero}</Text>
+            <Text>{absoluteFrecuencyFabian.positive}</Text>
+            <Text>{absoluteFrecuencyFabian.doublePositive}</Text> */}
+        </View>
+
+      <View style={styles.separator} />
+        <View>
+            <Text>{avgFabianDN.xGame}</Text>
+            <Text>{avgFabianN.xGame}</Text>
+            {/* <Text>{absoluteFrecuencyFabian.negative}</Text>
+            <Text>{absoluteFrecuencyFabian.zero}</Text>
+            <Text>{absoluteFrecuencyFabian.positive}</Text>
+            <Text>{absoluteFrecuencyFabian.doublePositive}</Text> */}
+        </View>     
 
       </View>
     </View>
--- functions/AbsoluteFrequency.ts
+++ functions/AbsoluteFrequency.ts
@@ -3,65 +3,137 @@
 // after a deep analyzing, we accord a method who compute the average of any action to value
 // only we need an array with all values, kind of action not matters
 
-function frqDoubleN (arr:number[]) : number {
-    let counter : number = 0;
-    for (let i:number = 0; i < arr.length ; i++){
+export function avgDoubleNegative (arr:number[]){
+    // we define the frecuency to calcule
+    let absoluteFrequency: number = 0;
+    let relativeFrecuency: number =0;
+
+    // this bucle calcule the fi to uses afer
+    for (let i= 0; i < arr.length ; i++){
         if (arr[i] === -2){
-            counter++;
+            absoluteFrequency++;
         }
     }
-    return counter;
-}
 
-function frqNegative(arr:number[]) : number{
-    let counter : number =0;
-    for (let i=0; i<arr.length;i++){
-        if (arr[i] === -1){
-            counter++;
-        }
-    }
-    return counter;
-}
+    // to calcule the relative frecuency, we need divide fi/N
+    //  where N is the length of arrays of values 
+    // F = f * N 
+    relativeFrecuency = (absoluteFrequency/ arr.length);
 
-function frqZero (arr : number []) : number {
-    let counter : number =0;
-    for (let i =0; i<arr.length; i++){
-        if (arr[i] === 0){
-            counter++;
-        }
-    }
-    return counter;
-}
+    // parcial represents the value of this action (-2), that means:
+    // parcial is the sum of all values in the array
+    let parcial: number = ((-2) * (absoluteFrequency));
 
-function frqPositive (arr : number []) : number {
-    let counter : number = 0;
-    for ( let i = 0; i<arr.length ; i++){
-        if(arr[i] === 1){
-            counter++;
-        }
-    }
-    return counter;
-}
+    // avg represent the percentage, that means:
+    // percentage that player fail only in this action 
+    let avg = (relativeFrecuency * 100);
 
-function frqDoubleP (arr: number []) : number {
-    let counter : number = 0;
-    for (let i=0; i<arr.length;i++){
-        if (arr[i] === 2) {
-            counter++;
-        }
-    }
-    return counter;
-}
-
-export function calculateFrecuancies (arr : number[]){
     return {
-        doubleNegative : frqDoubleN(arr),
-        negative : frqNegative (arr),
-        zero : frqZero (arr),
-        positive : frqPositive (arr),
-        doublePositive : frqDoubleP (arr)
+        absolute : absoluteFrequency,
+        relative : relativeFrecuency,
+        parcial,
+        xGame : avg
     }
 }
+
+export function avgNegative (arr : number []){
+    let absoluteFrequency : number = 0;
+    let relativeFrecuency : number =0;
+    
+    for (let i=0; i<arr.length; i++){
+        if (arr[i] === -1){
+            absoluteFrequency++;
+        }
+    }
+    relativeFrecuency = (absoluteFrequency/ arr.length);
+
+    // parcial represents the value of this action (-2), that means:
+    // parcial is the sum of all values in the array
+    let parcial: number = ((-2) * (absoluteFrequency));
+
+    // avg represent the percentage, that means:
+    // percentage that player fail only in this action 
+    let avg = (relativeFrecuency * 100);
+
+    return {
+        absolute : absoluteFrequency,
+        relative : relativeFrecuency,
+        parcial,
+        xGame : avg
+    }
+}
+
+
+
+
+
+
+
+
+
+function frqRelativeDoubleP (fi : number){
+    let hi : number =0
+}
+
+// function to compute all frequencytable
+
+
+
+
+
+
+
+
+
+// function frqNegative(arr:number[]) : number{
+//     let counter : number =0;
+//     for (let i=0; i<arr.length;i++){
+//         if (arr[i] === -1){
+//             counter++;
+//         }
+//     }
+//     return counter;
+// }
+
+// function frqZero (arr : number []) : number {
+//     let counter : number =0;
+//     for (let i =0; i<arr.length; i++){
+//         if (arr[i] === 0){
+//             counter++;
+//         }
+//     }
+//     return counter;
+// }
+
+// function frqPositive (arr : number []) : number {
+//     let counter : number = 0;
+//     for ( let i = 0; i<arr.length ; i++){
+//         if(arr[i] === 1){
+//             counter++;
+//         }
+//     }
+//     return counter;
+// }
+
+// function frqDoubleP (arr: number []) : number {
+//     let counter : number = 0;
+//     for (let i=0; i<arr.length;i++){
+//         if (arr[i] === 2) {
+//             counter++;
+//         }
+//     }
+//     return counter;
+// }
+
+// export function frecuencyAbs (arr : number[]){
+//     return {
+//         doubleNegative : frqDoubleN(arr),
+//         negative : frqNegative (arr),
+//         zero : frqZero (arr),
+//         positive : frqPositive (arr),
+//         doublePositive : frqDoubleP (arr)
+//     }
+// }
 
 // examples
 // from any array of values, get a frequency table with this values
@@ -69,7 +141,7 @@ export function calculateFrecuancies (arr : number[]){
 // 1.- [-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-1,-2,-1,-2]
 
 const AttacksFabian:number [] = [-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-1,-2,-1,-2];
-const absoluteFrequence= calculateFrecuancies(AttacksFabian);
-console.log(absoluteFrequence.negative)
+// const absoluteFrequence= frecuencyAbs(AttacksFabian);
+// console.log(absoluteFrequence.negative)
 
 
